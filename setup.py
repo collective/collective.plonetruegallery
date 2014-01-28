12a96fc5a3ed4fc9229f6e3b4d3353c4412604ff
hvelarde <hector.velarde@gmail.com>
2014-01-28 14:51:57 -0200

    Fix package distribution
    - Fix package classifiers
    - Fix package dependencies
    - Drop support for Plone 3.3 and Plone 4.0

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages
+from setuptools import setup
+
 import os
 
 version = "3.3.2"
@@ -10,31 +14,65 @@ setup(name='collective.plonetruegallery',
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Framework :: Plone :: 3.3",
-        "Framework :: Plone :: 4.0",
-        "Framework :: Plone :: 4.1",
-        "Framework :: Plone :: 4.2",
-        "Framework :: Plone :: 4.3"
-        ],
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Framework :: Plone :: 4.1',
+          'Framework :: Plone :: 4.2',
+          'Framework :: Plone :: 4.3'
+          'Framework :: Plone',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
       keywords='gallery plone slideshow photo photos image images picasa '
                'flickr highslide nivoslider nivogallery pikachoose fancybox supersized quicksand'
                'galleriffic galleria',
       author='Nathan Van Gheem',
       author_email='vangheem@gmail.com',
-      url='http://www.plone.org/products/plone-true-gallery',
-      license='GPL',
+      url='http://plone.org/products/plone-true-gallery',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
+          'plone.app.collection',
+          'plone.app.contentmenu',
+          'plone.app.contenttypes <=1.0',
+          'plone.app.form',
+          'plone.app.imaging',
+          'plone.app.portlets',
+          'plone.app.querystring',
+          'plone.app.vocabularies',
           'plone.app.z3cform',
-          'collective.ptg.galleria'
+          'plone.folder',
+          'plone.memoize',
+          'plone.portlets',
+          'plone.uuid',
+          'plone.z3cform',
+          'Products.Archetypes',
+          'Products.ATContentTypes',
+          'Products.CMFCore',
+          'Products.CMFPlone >=4.1',
+          'Products.GenericSetup',
+          'setuptools',
+          'transaction',
+          'z3c.form',
+          'zope.component',
+          'zope.i18nmessageid',
+          'zope.interface',
+          'zope.schema',
       ],
+      extras_require={
+          'test': [
+              'collective.ptg.galleria',
+              'plone.app.testing',
+              'plone.testing',
+              'unittest2',
+          ],
+      },
       entry_points="""
       # -*- Entry points: -*-
 
