e19b4db12b00086ed3c349603ecebd094dab5df1
Johannes Raggam <thetetet@gmail.com>
2018-03-01 15:13:10 +0100

    Extend the uninstall profile

--- collective/plonetruegallery/exportimport.py
+++ collective/plonetruegallery/exportimport.py
@@ -1,14 +1,16 @@
-from Products.CMFCore.utils import getToolByName
-from collective.plonetruegallery.settings import GallerySettings
 from collective.plonetruegallery.settings import ANNOTATION_KEY
+from collective.plonetruegallery.settings import GallerySettings
 from zope.annotation.interfaces import IAnnotations
 
 import json
+
 
 SETTINGS_FILENAME = 'gallerysettings.json'
 
 
 def install(context):
+    if not context.readDataFile('collective.plonetruegallery.txt'):
+        return
     site = context.getSite()
     default_settings = context.readDataFile(SETTINGS_FILENAME)
     if default_settings:
@@ -16,21 +18,6 @@ def install(context):
         settings = GallerySettings(site)
         for key, value in default_settings.items():
             setattr(settings, key, value)
-
-    if not context.readDataFile('collective.plonetruegallery.txt'):
-        return
-
-    types = getToolByName(site, 'portal_types')
-    if 'Large Plone Folder' in types.objectIds():
-        folder = types['Large Plone Folder']
-        view_methods = set(folder.view_methods)
-        view_methods.add('galleryview')
-        folder.view_methods = tuple(view_methods)
-    if 'Collection' in types.objectIds():
-        folder = types['Collection']
-        view_methods = set(folder.view_methods)
-        view_methods.add('galleryview')
-        folder.view_methods = tuple(view_methods)
 
 
 def export(context):
@@ -42,28 +29,3 @@ def export(context):
         context.writeDataFile(SETTINGS_FILENAME,
                               json.dumps(dict(settings), indent=4),
                               'application/json')
-
-
-def uninstall(context):
-    if not context.readDataFile('collective.plonetruegallery.uninstall.txt'):
-        return
-
-    portal = context.getSite()
-    portal_actions = getToolByName(portal, 'portal_actions')
-    object_buttons = portal_actions.object
-
-    actions_to_remove = ('gallery_settings', 'refresh-gallery')
-    for action in actions_to_remove:
-        if action in object_buttons.objectIds():
-            object_buttons.manage_delObjects([action])
-
-    #remove view
-    types_to_remove = ('Large Plone Folder', 'Folder', 'Topic', 'Collection')
-    types = getToolByName(portal, 'portal_types')
-
-    for _type in types_to_remove:
-        if _type in types.objectIds():
-            folder = types[_type]
-            view_methods = list(folder.view_methods)
-            view_methods.remove('galleryview')
-            folder.view_methods = tuple(view_methods)
